43fad34fd3e1cabda0883ecf563153112965e0cf
EscapeFromJava <reske.artur@gmail.com>
2022-08-13 00:00:31 +0300

    v 1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println("Is 'HelloWorld' equals? " + bean.equals(bean2) + "; links: " + bean + " : " + bean2);
+        System.out.println("Is 'Cat' equals? " + cat1.equals(cat2) + "; links: " + cat1 + " : " + cat2);
     }
 }
